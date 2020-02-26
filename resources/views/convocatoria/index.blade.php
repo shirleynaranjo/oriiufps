0bc751a208ec0df475dcabd4d087f7fcd4c02586
oriiufps <shirleypaolanv@ufps.edu.co>
2020-02-26 12:28:59 -0500

    Plantilla Convocatorias

---
+++ resources/views/convocatoria/index.blade.php
@@ -0,0 +1,109 @@
+@extends("theme.$theme.layout")
+@section('titulo')
+Convocatorias
+@endsection
+
+@section('content')
+
+@section('titulo1')
+Convocatorias
+@endsection
+
+@section('style')
+<link rel="stylesheet" href="{{asset("assets/$theme/plugins/datatables/css/dataTables.bootstrap.min.css")}}">
+<link rel="stylesheet" href="{{asset("assets/$theme/style/datatables.css")}}">
+@endsection
+
+@section('scripts')
+<script src="{{asset("assets/$theme/plugins/datatables/js/jquery.dataTables.min.js")}}"></script>
+<script src="{{asset("assets/$theme/plugins/datatables/js/dataTables.bootstrap.min.js")}}"></script>
+<script src="{{asset("assets/$theme/plugins/datatables/js/main.min.js")}}"></script>
+@endsection
+
+<div class="wrapper">
+    <!-- Inicio Header Convocatoria-->
+    @include("theme/$theme/headertitulo")
+    <!-- Fin Header Convocatoria-->
+</div>
+
+<!-- Inicio content-->
+<div class="container content profile">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box-body table-responsive no-padding">
+                <table id="table_noticia" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th style="width: 20%">Convocatoria</th>
+                            <th style="width: 10%">Oferente</th>
+                            <th>Fecha de cierre</th>
+                            <th>Vigencia</th>
+                            <th>Información</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Becas Orii</td>
+                            <td>UFPS</td>
+                            <td><i style="text-transform: none; color: #AA1916;">08/07</i></td>
+                            <td>2019</td>
+                            <td><i style="text-transform: none; color: #AA1916;">Publicación</i></td>
+                        </tr>
+                        <tr>
+                            <td>Becas Orii</td>
+                            <td>UFPS</td>
+                            <td><i style="text-transform: none; color: #AA1916;">08/07</i></td>
+                            <td>2019</td>
+                            <td><i style="text-transform: none; color: #AA1916;">Publicación</i></td>
+                        </tr>
+                        <tr>
+                            <td>Becas Orii</td>
+                            <td>UFPS</td>
+                            <td><i style="text-transform: none; color: #AA1916;">08/07</i></td>
+                            <td>2019</td>
+                            <td><i style="text-transform: none; color: #AA1916;">Publicación</i></td>
+                        </tr>
+                        <tr>
+                            <td>Becas Orii</td>
+                            <td>UFPS</td>
+                            <td><i style="text-transform: none; color: #AA1916;">08/07</i></td>
+                            <td>2019</td>
+                            <td><i style="text-transform: none; color: #AA1916;">Publicación</i></td>
+                        </tr>
+                        <tr>
+                            <td>Becas Orii</td>
+                            <td>UFPS</td>
+                            <td><i style="text-transform: none; color: #AA1916;">08/07</i></td>
+                            <td>2019</td>
+                            <td><i style="text-transform: none; color: #AA1916;">Publicación</i></td>
+                        </tr>
+                        <tr>
+                            <td>Becas Orii</td>
+                            <td>UFPS</td>
+                            <td><i style="text-transform: none; color: #AA1916;">08/07</i></td>
+                            <td>2019</td>
+                            <td><i style="text-transform: none; color: #AA1916;">Publicación</i></td>
+                        </tr>                       
+                        {{-- @foreach ($convocatorias as $convocatoria)
+                        <tr>
+                            <td>{{$convocatoria->nombreConvocatoria}}</td>
+                            <td>{{$convocatoria->oferenteConvocatoria}}</td>
+                            <td><i style="text-transform: none; color: #AA1916;">{{$convocatoria->fechaCierre}}</i></td>
+                            <td>{{$convocatoria->vigenciaConvocatoria}}</td>
+                            <td><i style="text-transform: none; color: #AA1916;">{{$convocatoria->informacion}}</i></td>
+                        </tr>
+                        @endforeach --}}
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    </div>
+    {{-- <div class="wrapper">
+        <!-- Inicio Header Noticia-->
+        @include("noticia/footerultimasnoticia")
+        <!-- Fin Header Noticias-->
+    </div> --}}
+</div>
+@endsection
