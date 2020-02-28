bf24c514e11c9319843d811c05969abe6c0c5929
oriiufps <shirleypaolanv@ufps.edu.co>
2020-02-28 07:08:40 -0500

    Migración y modelo Noticia

--- app/Models/Noticia.php
+++ app/Models/Noticia.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Noticia extends Model
 {
-    //
+    protected $table='noticia';
+    protected $primaryKey = 'idNoticia';
+    protected $fillable = ['tituloNoticia','fechaPublicacion','descripcionNoticia','imagen','video'];
+    protected $guarded = ['idNoticia'];
 }
---
+++ database/migrations/2020_02_28_115741_create_noticia_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoticiaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('noticia', function (Blueprint $table) {
+            $table->bigIncrements('idNoticia');
+            $table->string('tituloNoticia',500);     
+            $table->string('fechaPublicacion',500);       
+            $table->string('descripcionNoticia',5000);             
+            $table->string('imagen',200)->nullable();   
+            $table->string('video',200)->nullable();        
+            $table->timestamps();
+            $table->charset='utf8mb4';
+            $table->collation='utf8mb4_spanish2_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('noticia');
+    }
+}
