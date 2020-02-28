0524b71bec32c5e583b3b050d7c3452b6c4d6344
oriiufps <shirleypaolanv@ufps.edu.co>
2020-02-28 07:48:12 -0500

    Migracion y modelo convocatoria

--- app/Models/Convocatoria.php
+++ app/Models/Convocatoria.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Convocatoria extends Model
 {
-    //
+    protected $table='convocatoria';
+    protected $primaryKey = 'idConvocatoria';
+    protected $fillable = ['nombreConvocatoria','oferenteConvocatoria','vigenciaConvocatoria','informacionConvocatoria','estadoConvocatoria'];
+    protected $guarded = ['idConvocatoria'];
 }
---
+++ database/migrations/2020_02_28_123313_create_convocatoria_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConvocatoriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('convocatoria', function (Blueprint $table) {
+            $table->bigIncrements('idConvocatoria');
+            $table->string('nombreConvocatoria',200);
+            $table->string('oferenteConvocatoria',200);            
+            $table->string('vigenciaConvocatoria',200);
+            $table->string('informacionConvocatoria',200);
+            $table->string('estadoConvocatoria',100)->nullable();
+            $table->timestamps();
+            $table->charset='utf8mb4';
+            $table->collation='utf8mb4_spanish2_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('convocatoria');
+    }
+}
